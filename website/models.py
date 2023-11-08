1d6416bde260b8ec2ac49e117b425bb4c2f8a3e4
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-08 22:45:46 +0000

    Change the bookings model

--- website/models.py
+++ website/models.py
@@ -5,6 +5,22 @@ from cloudinary.models import CloudinaryField
 
 CONFIRMATION = ((0, 'Awaiting confirmation'), (1, 'Booking confirmed'), (2, 'Booking declined'))
 TABLE_SIZE = ((0, 'Single window seat'), (1, 'Small- 2 seats'), (2, 'Medium- 4 seats'), (3, 'Large- 6 seats'))
+TIME_SLOTS = (
+    ('8:00', '8:00'),
+    ('8:30', '8:30'),
+    ('9:00', '9:00'),
+    ('9:30', '9:30'),
+    ('10:00', '10:00'),
+    ('12:00', '12:00'),
+    ('12:30', '12:30'),
+    ('13:00', '13:00'),
+    ('13:30', '13:30'),
+    ('14:00', '14:00'),
+    ('14:30', '14:30'),
+    ('15:00', '15:00'),
+    ('15:30', '15:30'),
+    ('16:00', '16:00'),
+)
 
 
 # Extends base User model
@@ -17,6 +33,7 @@ class Customer(models.Model):
     contact_phone = models.CharField(max_length=17, null=True, blank=True)
     user_bookings = models.ForeignKey(Bookings, null=True, on_delete=models.CASCADE)
 
+
     def __str__(self):
         if self.user_id:
             return f'{self.first_name + " " + self.last_name}'
@@ -28,21 +45,14 @@ class Customer(models.Model):
 class Bookings(models.Model):
     booking_id = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE)
     booking_date = models.DateField()
-    booking_time = models.TimeField()
-    tables_booked = models.ForeignKey(Table)
+    booking_time = models.CharField(choices=TIME_SLOTS, default='8:00', blank=False)
+    tables_booked = models.IntegerField(choices=TABLE_SIZE, default=0)
     additional_info = models.TextField(max_length=400, null=True, blank=True)
-    is_confirmed = models.IntegerField(choices=CONFIRMATION, default=0)
+    is_confirmed = models.IntegerField(choices=CONFIRMATION, default=0)          
+
 
     def __str__(self):
         if self.booking_id:
             return f'Booking for {self.booking_id} currently has a status: {self.is_confirmed}'
         else:
             return 'Booking - User not specified'
-
-
-# Table size model -needs re-thinking
-# class Table(models.Model):
-#     size = models.IntegerField(choices=TABLE_SIZE, default=0)
-
-#     def __str__(self):
-#         if 
