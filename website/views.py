568bfd02223426ea771eab34b364f55e4f3418e8
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-14 00:41:21 +0000

    Add, style and connect homepage to views

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.template.response import TemplateResponse
 from django.views import generic, View
 from .models import Booking
 
 
-def home(request):
-    return render(request, 'index.html')
+def home(request, template_name="index.html"):
+    return TemplateResponse(
+        request, template_name, {'background': "background-lg"}
+        )
