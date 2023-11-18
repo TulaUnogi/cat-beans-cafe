658b07dc09a6a307a8353c027291e7e839fcfba3
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-18 16:34:06 +0000

    Update booking time in booking model with changed time slots

--- website/models.py
+++ website/models.py
@@ -6,20 +6,24 @@ from cloudinary.models import CloudinaryField
 CONFIRMATION = ((0, 'Awaiting confirmation'), (1, 'Booking confirmed'), (2, 'Booking declined'))
 TABLE_SIZE = ((0, 'Single window seat'), (1, 'Small- 2 seats'), (2, 'Medium- 4 seats'), (3, 'Large- 6 seats'))
 TIME_SLOTS = (
-    ('8:00', '8:00'),
-    ('8:30', '8:30'),
-    ('9:00', '9:00'),
-    ('9:30', '9:30'),
-    ('10:00', '10:00'),
-    ('12:00', '12:00'),
-    ('12:30', '12:30'),
-    ('13:00', '13:00'),
-    ('13:30', '13:30'),
-    ('14:00', '14:00'),
-    ('14:30', '14:30'),
-    ('15:00', '15:00'),
-    ('15:30', '15:30'),
-    ('16:00', '16:00'),
+    (0, '8:00 - 8:30'),
+    (1, '8:30 - 9:00'),
+    (2, '9:00 - 9:30'),
+    (3, '9:30 - 10:00'),
+    (4, '10:00 - 10:30'),
+    (5, '10:30 - 11:00'),
+    (6, '11:00 - 11:30'),
+    (7, '11:30 - 12:00'),
+    (8, '12:00 - 12:30'),
+    (9, '12:30 - 13:00'),
+    (10, '13:00 - 13:30'),
+    (11, '13:30 - 14:00'),
+    (12, '14:00 - 14:30'),
+    (13, '14:30 - 15:00'),
+    (17, '15:00 - 15:30'),
+    (18, '15:30 - 16:00'),
+    (19, '16:00 - 16:30'),
+    (20, '16:30 - 17:00')
 )
 
 
@@ -45,7 +49,7 @@ class Booking(models.Model):
 
     booking_customer = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE)
     booking_date = models.DateField()
-    booking_time = models.CharField(choices=TIME_SLOTS, default='8:00', blank=False, max_length=5)
+    booking_time = models.IntegerField(choices=TIME_SLOTS, default='8:00', blank=False)
     tables_booked = models.IntegerField(choices=TABLE_SIZE, default=0)
     additional_info = models.TextField(max_length=400, null=True, blank=True)
     booked_on = models.DateTimeField(auto_now_add=True)
