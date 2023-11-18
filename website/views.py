9c2810acb79c9212cc4bd9f4195ca33ad19831d8
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-18 19:56:21 +0000

    Remove TemplateResponse and rendering backgrounds

--- website/views.py
+++ website/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.template.response import TemplateResponse
 from django.views import generic, View
 from .models import Booking
 
 
 def home(request, template_name="index.html"):
-    return TemplateResponse(
-        request, template_name, {'background': "background-lg"}
+    return render(
+        request, template_name
     )
 
 def about(request, template_name="about.html"):
-    return TemplateResponse(
-        request, template_name, {'background': "background-plain"}
+    return render(
+        request, template_name,
     )
 
 def booking_form(request, template_name="booking-form.html"):
-    return TemplateResponse(
-        request, template_name, {'background': "background-lg"}
+    return render(
+        request, template_name,
     )
