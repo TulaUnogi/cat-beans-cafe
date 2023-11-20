b2679f02a397443ae4f0e1668e0cb0cd3ade6684
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-20 20:59:48 +0000

    Install crispy-forms

--- catbeanscafe/settings.py
+++ catbeanscafe/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'website',
 ]
 
@@ -61,6 +63,9 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
