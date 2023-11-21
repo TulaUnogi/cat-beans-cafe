d5fb553ce49ae5db84a987df6fbfc5cbca7b8ea0
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-21 22:47:56 +0000

    Fix the indentation in BookingForm

--- website/forms.py
+++ website/forms.py
@@ -1,4 +1,4 @@
-from .models import Customer, Booking
+from .models import UserProfile, Booking
 from django import forms
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
@@ -6,7 +6,7 @@ from crispy_forms.layout import Submit
 from django.urls import reverse
 
 
-class BookingForm(forms.ModelForm):
+class BookingForm(ModelForm):
     """ 
     Provides necessary fields to the booking form for customers
     """
@@ -20,12 +20,13 @@ class BookingForm(forms.ModelForm):
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit'))
 
-        # Provides a date widget to the form 
-        booking_date = forms.DateField(widget=forms.DateInput, required=True)
+    # Provides a date widget to the form 
+    booking_date = forms.DateField(widget=forms.DateInput, required=True)
 
 
-        # Provides a model to pull the fields from
-        class Meta:
-            model = Booking
-            fields = ['booking_date', 'booking_time', 'tables_booked', 'additional_info']
+    # Provides a model to pull the fields from
+    class Meta:
+        model = Booking
+        fields = ['booking_date', 'booking_time', 'tables_booked', 'additional_info']
+
 
