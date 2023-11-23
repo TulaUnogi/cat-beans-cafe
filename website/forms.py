bd22297cbcaf49fe621e145f8907183e4edea2a6
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-23 00:14:09 +0000

    Change values in Profile & Booking, validate dates

--- website/forms.py
+++ website/forms.py
@@ -1,15 +1,19 @@
+import datetime
 from .models import UserProfile, Booking
 from django import forms
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.urls import reverse
+from django_summernote.widgets import SummernoteInplaceWidget
+from .models import TABLE_SIZE, TIME_SLOTS
 
 
 class BookingForm(ModelForm):
     """ 
     Provides necessary fields to the booking form for customers
     """
+
     def __init__(self, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
@@ -21,13 +25,27 @@ class BookingForm(ModelForm):
         self.helper.add_input(Submit('submit', 'Submit'))
 
     # Provides a date widget to the form 
-    booking_date = forms.DateField(widget=forms.DateInput, required=True)
+    booking_date = forms.DateField(widget=forms.DateInput, initial=datetime.date.today, required=True)
+    booking_time = forms.ChoiceField(choices=TIME_SLOTS, initial=["8:00 - 8:30",], required=True)
+    tables_booked = forms.MultipleChoiceField(choices=TABLE_SIZE, initial="Single window seat", required=True)
+    additional_info = forms.CharField(max_length=400, widget=SummernoteInplaceWidget())
 
 
     # Provides a model to pull the fields from
     class Meta:
         model = Booking
         fields = ['booking_date', 'booking_time', 'tables_booked', 'additional_info']
+
+
+    # Prevents booking dates in the past
+    def save(self, *args, **kwargs):
+
+        if self.date < datetime.date.today():
+            raise ValidationError("The date cannot be in the past!")
+        else:
+            super(Booking, self).save(*args, **kwargs)
+
+
 
 
 class ProfileForm(ModelForm):
@@ -45,6 +63,11 @@ class ProfileForm(ModelForm):
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit'))
     
+    
+    first_name = forms.CharField(required=True, max_length=50)
+    last_name = forms.CharField(required=True, max_length=50)
+    phone_number = forms.CharField(required=True, max_length=17)
+    email = forms.EmailField(required=True, max_length=300)
 
     # User data fields
     class Meta:
