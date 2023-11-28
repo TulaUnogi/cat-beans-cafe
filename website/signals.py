216c02dd4d38ae0c6fcb23c780c5da59509831ba
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-28 00:11:42 +0000

    Add signals

---
+++ website/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import UserProfile
+
+
+@receiver(post_save, sender=User)
+def create_and_edit_profile(sender, instance, created, **kwargs):
+    """
+    Signal to create or update the user profile. 
+    Thanks to my tutor Jason.
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    
+    instance.userprofile.save()
