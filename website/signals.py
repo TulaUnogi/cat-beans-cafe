000400967286beb2ed7b63456163c843ae6bcaac
Karolina Piech <karolinapiech1990@gmail.com>
2023-11-29 16:15:32 +0000

    Uncomment and alter create method

--- website/signals.py
+++ website/signals.py
@@ -1,16 +1,17 @@
-# from django.db.models.signals import post_save
-# from django.contrib.auth.models import User
-# from django.dispatch import receiver
-# from .models import UserProfile
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import UserProfile
 
 
-# @receiver(post_save, sender=User)
-# def create_and_edit_profile(sender, instance, created, **kwargs):
-#     """
-#     Signal to create or update the user profile. 
-#     Thanks to my tutor Jason.
-#     """
-#     if created:
-#         User.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_and_edit_profile(sender, instance, created, **kwargs):
+    """
+    Signal to create or update the user profile. 
+    Thanks to my tutors Jason and Oisin.
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+
     
-#     instance.user.save()
+    instance.userprofile.save()
