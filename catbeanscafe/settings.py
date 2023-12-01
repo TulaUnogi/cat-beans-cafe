a3cd19852e2db86b4ec4f75952862ca9d071b4c2
Karolina Piech <karolinapiech1990@gmail.com>
2023-12-01 10:04:52 +0000

    Add CLOUDINARY_URL to settings.py explicitly

--- catbeanscafe/settings.py
+++ catbeanscafe/settings.py
@@ -180,6 +180,8 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+CLOUDINARY_URL = os.environ['CLOUDINARY_URL']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
